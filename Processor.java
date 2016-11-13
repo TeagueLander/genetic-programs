f03b2f0045d13d1620aacd473968c780c36bae2a
Teague Lander <tlander121@hotmail.com>
2016-11-13 13:39:22 -0800

    Clean up

--- Processor.java
+++ Processor.java
@@ -1,26 +1,33 @@
 
+import java.util.Stack;
+
 public class Processor {
 	
-	//maybe add NOT?
-	public static final int NOP		= 0b0000;
-	public static final int ADD		= 0b0001;
-	public static final int SUB		= 0b0010;
+	//Math operations (add NOT?)
+	public static final byte NOP	= 0b0000;
+	public static final byte ADD	= 0b0001;
+	public static final byte SUB	= 0b0010;
+	//Bit operations
+	public static final byte AND	= 0b0100;
+	public static final byte OR		= 0b0101;
+	public static final byte SHL	= 0b0110;
+	public static final byte SHR	= 0b0111;
+	//Stack operations
+	public static final byte PUSH1	= 0b1000;
+	public static final byte POP1	= 0b1001;
+	public static final byte PUSH2	= 0b1010;
+	public static final byte POP2	= 0b1011;
+	//Input output
+	public static final byte OUT	= 0b1100;
 	
-	public static final int AND		= 0b0100;
-	public static final int OR		= 0b0101;
-	public static final int SHL		= 0b0110;
-	public static final int SHR		= 0b0111;
-	
-	public static final int PUSH1	= 0b1000;
-	public static final int POP1	= 0b1001;
-	public static final int PUSH2	= 0b1010;
-	public static final int POP2	= 0b1011;
-	
-	public static final int OUT		= 0b1100;
 	
 	
 	public Processor() {
 		
+	}
+	
+	public static void main(String[] args) {
+		Processor processor = new Processor();
 	}
 	
 }
